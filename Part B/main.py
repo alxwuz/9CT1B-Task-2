9bd9ba70a190bc94fd93143f0a66604c80740730
alxwuz <alex.wu24@education.nsw.gov.au>
2024-08-20 22:44:15 +1000

    work

--- Part B/main.py
+++ Part B/main.py
@@ -7,18 +7,19 @@ quit = False
 
 # Set up the df
 original_df = pd.read_csv('Part B/data/imdb_top_1000.csv')
-3
 updated_df = pd.read_csv('Part B/data/imdb_top_1000.csv',
-                            header=None,
-                            names=['Poster_link', 'Series_Title', 'Released_Year', 'Certificate', 'Runtime', 'Genre', 'IMDB_Rating', 'Overview', 'Meta_score', 'Director', 'Star1', 'Star2', 'Star3', 'Star4', 'No_of_votes', 'Gross'])
-updated_df = updated_df.drop(columns=['Poster_link', 'Director', 'Star1', 'Star2', 'Star3', 'Star4', 'No_of_votes', 'Gross'])
+                         header=None,
+                         names=['Series_Title', 'Released_Year', 'Certificate', 'Runtime', 'Genre', 'IMDB_Rating', 'Overview', 'Meta_score'])
+
+columns_to_drop = ['Poster_Link', 'Other_Column', 'Director', 'Star1', 'Star2', 'Star3', 'Star4', 'No_of_Votes', 'Gross']
+updated_df = original_df.drop(columns=columns_to_drop, errors='ignore')
 
 # Define the functions
 def showData():
-    original_df
+    print(original_df)
 
 def updatedData():
-    updated_df
+    print(updated_df)
 
 def userOptions():
     global quit
@@ -28,6 +29,7 @@ def userOptions():
      Welcome to Alex's dataset sorter! Please select an option:
      1: Show the original dataset
      2: Show the updated dataset
+     3: Visualise x in a graph
      10: Quit
      
      """)
@@ -36,15 +38,16 @@ def userOptions():
         answer = int(input('Enter input: '))
 
         if answer == 1:
-            print(showData)
+            showData()
         elif answer == 2:
-            print(updatedData)
+            updatedData()
         elif answer == 10:
             quit = True
+
         else:
             print('Incorrect syntax. Please enter a number between 1 and x.')
 
-    except:
+    except ValueError:
         print('Invalid syntax, enter a number >:(.')
 
 while not quit:
